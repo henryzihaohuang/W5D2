b7b2ddcf8481b6dc96a9705afafd14b9a78855fa
henryzihaohuang <atlas.bb.contact@gmail.com>
2020-11-24 14:46:30 -0600

    Finish 03_select_nobel.rb

--- skeleton/lib/03_select_nobel.rb
+++ skeleton/lib/03_select_nobel.rb
@@ -48,6 +48,12 @@ def einstein_prize
   # Show the year and subject that won 'Albert Einstein' his prize.
   execute(<<-SQL)
     SELECT 
+      yr, subject
+    FROM
+      nobels
+    WHERE
+      winner = 'Albert Einstein';
+    
       
   SQL
 end
@@ -55,6 +61,13 @@ end
 def millennial_peace_prizes
   # Give the name of the 'Peace' winners since the year 2000, including 2000.
   execute(<<-SQL)
+  SELECT
+    winner
+  FROM
+    nobels
+  WHERE
+    subject = 'Peace' AND
+    yr BETWEEN 2000 AND 2008;
   SQL
 end
 
@@ -62,6 +75,16 @@ def eighties_literature
   # Show all details (yr, subject, winner) of the Literature prize winners
   # for 1980 to 1989 inclusive.
   execute(<<-SQL)
+
+    SELECT
+      yr, subject, winner
+    FROM
+      nobels
+    WHERE
+      subject = 'Literature' AND
+      yr BETWEEN 1980 AND 1989;
+
+    
   SQL
 end
 
@@ -69,11 +92,24 @@ def presidential_prizes
   # Show all details of the presidential winners: ('Theodore Roosevelt',
   # 'Woodrow Wilson', 'Jimmy Carter')
   execute(<<-SQL)
+  SELECT
+    yr, subject, winner
+  FROM
+    nobels
+  WHERE
+    winner IN('Theodore Roosevelt','Woodrow Wilson', 'Jimmy Carter');
   SQL
 end
 
 def nobel_johns
   # Show the winners with first name John
   execute(<<-SQL)
+  SELECT 
+    winner
+  FROM
+    nobels
+  WHERE
+    winner LIKE 'John%';
+
   SQL
 end
